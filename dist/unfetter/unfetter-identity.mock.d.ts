1dbeaabcc4e95c242557e56d5e253a7f79e4f02b
mushinlogit <mushinlogit@protonmail.com>
2018-04-12 16:01:08 -0400

    add identity mock

---
+++ dist/unfetter/unfetter-identity.mock.d.ts
@@ -0,0 +1,6 @@
+import { Mock } from '../mock';
+import { UnfetterIdentity } from './unfetter-identity';
+export declare class UnfetterIdentityMock extends Mock<UnfetterIdentity> {
+    mockOne(): UnfetterIdentity;
+}
+export declare const UnfetterIdentityMockFactory: UnfetterIdentityMock;

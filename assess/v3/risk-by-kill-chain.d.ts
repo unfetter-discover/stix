824d6df41a842a32771fe010322b1770ef94abc8
mpmmasterpeace <24652622+mpmmasterpeace@users.noreply.github.com>
2018-06-08 07:26:53 -0400

    updating models to support changes for v3 assessment summaries
    supports unfetter-discover/unfetter#1084

---
+++ assess/v3/risk-by-kill-chain.d.ts
@@ -0,0 +1,10 @@
+import { AssessKillChainType } from '../v2/assess-kill-chain-type';
+/**
+ * @description kill chain data for risk analysis
+ */
+export declare class RiskByKillChain {
+    courseOfActions: AssessKillChainType[];
+    indicators: AssessKillChainType[];
+    sensors: AssessKillChainType[];
+    capabilities: AssessKillChainType[];
+}
---
+++ assess/v3/risk-by-kill-chain.js
@@ -0,0 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @description kill chain data for risk analysis
+ */
+var RiskByKillChain = /** @class */ (function () {
+    function RiskByKillChain() {
+        this.courseOfActions = [];
+        this.indicators = [];
+        this.sensors = [];
+        this.capabilities = [];
+    }
+    return RiskByKillChain;
+}());
+exports.RiskByKillChain = RiskByKillChain;

73b83ea10725ebe15b0a12c1885a87bd1093aebc
mpmmasterpeace <24652622+mpmmasterpeace@users.noreply.github.com>
2018-06-11 12:11:46 -0400

    needed to change/add source files not just the compiled js files

--- src/assess/v3/index.ts
+++ src/assess/v3/index.ts
@@ -2,3 +2,4 @@ export * from './baseline';
 export { Assess3Meta } from './assess3-meta';
 export { Assessment } from './assessment';
 export { AssessmentEvalTypeEnum } from './assessment-eval-type.enum';
+export { RiskByKillChain } from './risk-by-kill-chain';

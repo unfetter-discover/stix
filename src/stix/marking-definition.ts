81414607843bf84c6b346b5e555f7cde461148a1
j987987 <j987897@users.noreply.github.com>
2018-08-06 12:58:38 -0400

    update marking definition

--- src/stix/marking-definition.ts
+++ src/stix/marking-definition.ts
@@ -2,7 +2,7 @@ import { StixCoreEnum } from './stix-core.enum';
 
 export class MarkingDefinition {
     public definition_type: string;
-    public definition = {} as { rating: number, label: string };
+    public definition: any = {};
     public created: Date;
     public readonly type = StixCoreEnum.MARKING_DEFINITION;
     public id: string;

1e116bfed4e5272c656d194f86af992eaa0e1a4b
j987987 <j987897@users.noreply.github.com>
2018-08-06 15:02:31 -0400

    markign definition extends stixcore

--- src/stix/marking-definition.ts
+++ src/stix/marking-definition.ts
@@ -1,9 +1,12 @@
+import { StixCore } from './stix-core';
 import { StixCoreEnum } from './stix-core.enum';
 
-export class MarkingDefinition {
+export class MarkingDefinition extends StixCore {
     public definition_type: string;
     public definition: any = {};
-    public created: Date;
-    public readonly type = StixCoreEnum.MARKING_DEFINITION;
-    public id: string;
+
+    constructor(data?: StixCore) {
+        super(data);
+        this.type = StixCoreEnum.MARKING_DEFINITION;
+    }
 }
